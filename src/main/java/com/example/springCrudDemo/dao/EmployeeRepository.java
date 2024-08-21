2524dde55f0d53c262f5a692d38eb23f245d6e1e
tranlam11 <hayashi6714@gmail.com>
2024-08-21 17:07:37 +0700

    spring crud with sprin data rest

--- src/main/java/com/example/springCrudDemo/dao/EmployeeRepository.java
+++ src/main/java/com/example/springCrudDemo/dao/EmployeeRepository.java
@@ -2,9 +2,10 @@ package com.example.springCrudDemo.dao;
 
 import com.example.springCrudDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer>  {
     // Additional methods can be added here if needed.
-
 
 }
