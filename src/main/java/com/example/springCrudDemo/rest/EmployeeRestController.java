6976b514fe13bd571d5151f9d48bc1003cb5424d
tranlam11 <hayashi6714@gmail.com>
2024-09-13 17:20:40 +0700

    BUILD: API for read employees from dbs

--- src/main/java/com/example/springCrudDemo/rest/EmployeeRestController.java
+++ src/main/java/com/example/springCrudDemo/rest/EmployeeRestController.java
@@ -1,23 +1,13 @@
 package com.example.springCrudDemo.rest;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.springCrudDemo.entity.Employee;
 import com.example.springCrudDemo.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/employees")
+@RequestMapping("/api")
 @CrossOrigin(origins = "http://127.0.0.1:5500")
 public class EmployeeRestController {
 
@@ -33,8 +23,6 @@ public class EmployeeRestController {
 //        return employeeDao.findAll();
 //    }
 //}
-
-
     private EmployeeService employeeService;
 
     public EmployeeRestController(EmployeeService employeeService) {
@@ -57,13 +45,11 @@ public class EmployeeRestController {
     	if (theEmployee == null) {
     		throw new RuntimeException("Employee not found - " + employeeId); 
     	}
-    	
     	return theEmployee;
     }
     
     @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee theEmployee) {
-    	
     	//phòng trừ case pass id qua JSON thì nên setID thành 0
     	//này cũng tương đương ép buộc tạo mới, thay vì update (vì nếu pass nhầm id tồn tại thành ra sửa)
     	theEmployee.setId(0);
@@ -72,6 +58,7 @@ public class EmployeeRestController {
     	
     	return dbEmployee;
     }
+
     
     @PutMapping("/employees")
     public Employee updateEmployee(@RequestBody Employee theEmployee) {
@@ -79,9 +66,9 @@ public class EmployeeRestController {
     	Employee updateEmployee = employeeService.save(theEmployee);
     	return updateEmployee;
     }
+
     
-    
-    @DeleteMapping("/employees/{employeeId}")
+    @DeleteMapping("/delete/{employeeId}")
     public String deleteEmployee (@PathVariable int employeeId) {
     	
     	//Take employeeId want to delete
